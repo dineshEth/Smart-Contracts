18493a7851d9dfe669c9c08833dec122206ae112
Dinesh <dineshchauhan416pd@gmail.com>
2022-07-14 22:50:31 +0530

    Transfer function and get tokens with BalanceOf function is added

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,5 +2,31 @@
 pragma solidity >= 0.5.0 < 0.9.0;
 
 contract Token {
-    
+    string public name =  "My Token";
+    string public symbol  = "MT";
+    uint256 public totalSupply = 10000;
+
+    address public owner;
+    mapping (address => uint256) balances;
+     
+     constructor () {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+     }
+     //modifier
+     modifier onlyOwner(){
+        require(owner==msg.sender, "you're not owner");
+        _;
+     }
+
+     //function to transfer 
+     function transfer(address to , uint256 amount) external onlyOwner{
+        require(balances[msg.sender]>=amount ,"not enough tokens");
+        balances[msg.sender] -=amount;
+        balances[to] +=amount;
+     }
+    // function to get balance of
+     function balanceOf(address account) external onlyOwner view returns(uint){
+        return balances[account];
+     }
 }
